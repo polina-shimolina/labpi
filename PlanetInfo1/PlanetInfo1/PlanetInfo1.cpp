6952d894e989cd2280ada3ca1d212bb6d7f17eb4
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2021-03-29 21:39:03 +0300

    3

--- PlanetInfo1/PlanetInfo1/PlanetInfo1.cpp
+++ PlanetInfo1/PlanetInfo1/PlanetInfo1.cpp
@@ -72,13 +72,23 @@ bool compareFunction(std::string a, std::string b) { return a < b; }
 
 void sort(Planet* p_l, int* s, int N) {
     Planet temp;
+    int tempint;
     for (int i = 0; i < N - 1; i++) {
+        
         for (int j = 0; j < N - 1 - i; j++) {
+            
+            
             if (s[j] > s[j + 1]) {
+                tempint = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = tempint;
                 temp = p_l[j];
                 p_l[j] = p_l[j + 1];
                 p_l[j + 1] = temp;
+
+                
             }
+            
         }
     }
 }
